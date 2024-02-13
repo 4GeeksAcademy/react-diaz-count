0aecab3df9e6db701b25debb2e7462a1ba95074e
Santiago Diaz Muñoz <149324836+Diaz1010@users.noreply.github.com>
2024-02-13 00:56:44 +0000

    initial

---
+++ src/js/component/Cards.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {FaClock} from 
+
+function Cards(props) {
+    return
+    
+}
+export default Cards
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,10 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+let counter = 0
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+//render your react application+
+setInterval(() => {
+    counter = counter+1
+    ReactDOM.render(<Home seconds={counter} />, document.querySelector("#app"));
+},1000)
